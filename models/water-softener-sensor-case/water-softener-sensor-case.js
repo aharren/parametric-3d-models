e2a0007b573e42bc7915314eef5eb4d162f79474
Arne Harren <ah@0xc0.de>
2026-02-19 09:02:48 +0100

    extract some grid layout helpers

--- models/water-softener-sensor-case/water-softener-sensor-case.js
+++ models/water-softener-sensor-case/water-softener-sensor-case.js
@@ -5,13 +5,8 @@ const { cylinder, cuboid } = require('@jscad/modeling').primitives;
 const { subtract, union } = require('@jscad/modeling').booleans;
 
 const { openBox, lid } = require('../../lib/boxes');
+const grid = require('../../lib/grid');
 const preview = require('../../lib/preview');
-
-const at = (pos, obj) => {
-  // align corner of object relative to [0,0,0]
-  const zero = align({ modes: ['min', 'min', 'min'] }, obj);
-  return translate(pos, zero);
-}
 
 const main = (params) => {
   const espBoardSizeX = 31.5;
@@ -38,21 +33,21 @@ const main = (params) => {
   const segments = 64;
 
   const enclosureBox = () => {
-    const outerBox = at([-wallThickness, -wallThickness, -wallThickness], openBox({ size: enclosureSize, wallThickness, segments, outerCornerRadius }));
+    const outerBox = grid.at([-wallThickness, -wallThickness, -wallThickness], openBox({ size: enclosureSize, wallThickness, segments, outerCornerRadius }));
 
     const bracketHeight = 4;
-    const bracket1 = at([espBoardSizeX, 0, 0], cuboid({ size: [wallThickness, innerSize[1], bracketHeight] }));
-    const bracket2 = at([espBoardSizeX + espTofBoardDistanceX - wallThickness, 0, 0], cuboid({ size: [wallThickness, innerSize[1], bracketHeight] }));
+    const bracket1 = grid.at([espBoardSizeX, 0, 0], cuboid({ size: [wallThickness, innerSize[1], bracketHeight] }));
+    const bracket2 = grid.at([espBoardSizeX + espTofBoardDistanceX - wallThickness, 0, 0], cuboid({ size: [wallThickness, innerSize[1], bracketHeight] }));
 
-    const usbConnectorHole = at([espBoardSizeX / 2 - espUsbConnectorWidth / 2, -wallThickness, 2], cuboid({ size: [espUsbConnectorWidth, wallThickness, espUsbConnectorHeight] }));
+    const usbConnectorHole = grid.at([espBoardSizeX / 2 - espUsbConnectorWidth / 2, -wallThickness, 2], cuboid({ size: [espUsbConnectorWidth, wallThickness, espUsbConnectorHeight] }));
 
     const tofBoardMountingHeight = innerSizeHeight - 1;
-    const tofBoardMounting1 = at([espBoardSizeX + espTofBoardDistanceX + tofBoardSizeX / 2 - tofBoardMountingHoleRadius, innerSize[1] / 2 - tofBoardMountingHolesDistance / 2 - tofBoardMountingHoleRadius, 0], cylinder({ radius: tofBoardMountingHoleRadius, height: tofBoardMountingHeight }));
-    const tofBoardMounting2 = at([espBoardSizeX + espTofBoardDistanceX + tofBoardSizeX / 2 - tofBoardMountingHoleRadius, innerSize[1] / 2 + tofBoardMountingHolesDistance / 2 - tofBoardMountingHoleRadius, 0], cylinder({ radius: tofBoardMountingHoleRadius, height: tofBoardMountingHeight }));
+    const tofBoardMounting1 = grid.at([espBoardSizeX + espTofBoardDistanceX + tofBoardSizeX / 2 - tofBoardMountingHoleRadius, innerSize[1] / 2 - tofBoardMountingHolesDistance / 2 - tofBoardMountingHoleRadius, 0], cylinder({ radius: tofBoardMountingHoleRadius, height: tofBoardMountingHeight }));
+    const tofBoardMounting2 = grid.at([espBoardSizeX + espTofBoardDistanceX + tofBoardSizeX / 2 - tofBoardMountingHoleRadius, innerSize[1] / 2 + tofBoardMountingHolesDistance / 2 - tofBoardMountingHoleRadius, 0], cylinder({ radius: tofBoardMountingHoleRadius, height: tofBoardMountingHeight }));
 
-    const tofSensorHole = at([espBoardSizeX + espTofBoardDistanceX + tofBoardSizeX / 2 - tofBoardMountingHoleRadius - tofSensorViewSizeX / 2, innerSize[1] / 2 - tofSensorViewSizeY / 2, -wallThickness], cuboid({ size: [tofSensorViewSizeX, tofSensorViewSizeY, wallThickness] }));
+    const tofSensorHole = grid.at([espBoardSizeX + espTofBoardDistanceX + tofBoardSizeX / 2 - tofBoardMountingHoleRadius - tofSensorViewSizeX / 2, innerSize[1] / 2 - tofSensorViewSizeY / 2, -wallThickness], cuboid({ size: [tofSensorViewSizeX, tofSensorViewSizeY, wallThickness] }));
 
-    return subtract(
+    const model = subtract(
       union(
         outerBox,
         bracket1,
@@ -63,34 +58,36 @@ const main = (params) => {
       usbConnectorHole,
       tofSensorHole,
     );
+    return grid.center(model);
   }
 
   const enclosureLid = () => {
-    const lidBase = at([-wallThickness, -wallThickness, 0], lid({ size: enclosureSize, wallThickness, segments, outerCornerRadius }));
+    const lidBase = grid.at([-wallThickness, -wallThickness, 0], lid({ size: enclosureSize, wallThickness, segments, outerCornerRadius }));
 
     const airHoleWidth = 1;
     const airHoleLength = innerSize[1] / 2;
     const airHoles = union(
-      at([innerSize[0] / 20 * 2, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 3, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 4, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 5, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 6, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 7, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 8, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 17, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
-      at([innerSize[0] / 20 * 18, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 2, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 3, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 4, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 5, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 6, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 7, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 8, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 17, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
+      grid.at([innerSize[0] / 20 * 18, innerSize[1] / 2 - airHoleLength / 2, 0], cuboid({ size: [airHoleWidth, airHoleLength, wallThickness] })),
     );
 
-    return subtract(
+    const model = subtract(
       lidBase,
       airHoles
-    )
+    );
+    return grid.center(model);
   };
 
   return [
-    translate([0, 10, 0], align({ modes: ['center', 'min', 'min'] }, enclosureBox())),
-    translate([0, -10, 0], align({ modes: ['center', 'max', 'min'] }, enclosureLid())),
+    translate([0, 10, 0], align({ modes: ['none', 'min', 'none'] }, enclosureBox())),
+    translate([0, -10, 0], align({ modes: ['none', 'max', 'none'] }, enclosureLid())),
   ];
 }
 
