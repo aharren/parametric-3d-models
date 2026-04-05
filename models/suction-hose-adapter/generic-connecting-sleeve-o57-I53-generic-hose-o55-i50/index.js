163b223a807fbd6a2a8b0a711d48ebee234d2dc4
Arne Harren <ah@0xc0.de>
2026-04-05 17:30:30 +0200

    add suction hose adapter for generic 57mm plug and 50mm hose

---
+++ models/suction-hose-adapter/generic-connecting-sleeve-o57-I53-generic-hose-o55-i50/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { degToRad } = require('@jscad/modeling/src').utils;
+
+const connectors = require('../../lib/suction-hose-connectors');
+
+const config = {
+  connector1: connectors.invert({ play: 0.0 }, connectors.library.generic.ConnectingSleeve_O57_I53.plug),
+  connector2: connectors.invert({ play: 0.0 }, connectors.library.generic.Hose_O55_I50.socket),
+  bendAngle: degToRad(0),
+  wallThickness: 2,
+  segments: 64,
+};
+
+module.exports = config;
