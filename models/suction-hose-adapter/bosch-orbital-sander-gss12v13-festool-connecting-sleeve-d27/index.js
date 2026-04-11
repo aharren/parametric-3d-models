e4225d5c301545313d1b081e2f8e8fcb1143c365
Arne Harren <ah@0xc0.de>
2026-04-11 19:30:46 +0200

    update angle in bosch sander/ festool suction hose adapter

--- models/suction-hose-adapter/bosch-orbital-sander-gss12v13-festool-connecting-sleeve-d27/index.js
+++ models/suction-hose-adapter/bosch-orbital-sander-gss12v13-festool-connecting-sleeve-d27/index.js
@@ -7,7 +7,7 @@ const connectors = require('../../lib/suction-hose-connectors');
 const config = {
   connector1: connectors.invert({ play: 0.0 }, connectors.library.bosch.OrbitalSander_GSS12V13_O28.plug),
   connector2: connectors.invert({ play: 0.0 }, connectors.library.festool.ConnectingSleeve_D27_O34_I27.plug),
-  bendAngle: degToRad(30),
+  bendAngle: degToRad(40),
   wallThickness: 2,
   segments: 64,
 };
