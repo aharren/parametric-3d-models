bb16da8827aedb433e3960cc2e3c8938a1f1ce4c
Arne Harren <ah@0xc0.de>
2026-04-18 20:04:18 +0200

    add vertical device holder model for Ubiquiti UK Ultra / Swiss Army Knife access point

---
+++ models/ikea-skadis-vertical-device-holder/ubiquiti-uk-ultra/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Ubiquiti UK Ultra / Swiss Army Knife Access Point
+const config = {
+  deviceHeight: 33.2,
+  deviceWidth: 84,
+  deviceDepthEnclosed: 25,
+  bottomBorderWidth: 20,
+
+  wallThickness: 4,
+
+  numBracketsX: 2,
+  numBracketsY: 2,
+  bracketWidth: 2,
+  bracketDepth: 2,
+};
+
+module.exports = require('../../../lib/submodel').exports(require('../ikea-skadis-vertical-device-holder'), config);
