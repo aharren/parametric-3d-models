74d2562520ce10b9c6d4d9c571ed4b2fb57e3ccf
Arne Harren <ah@0xc0.de>
2026-04-19 15:04:12 +0200

    add suction hose connector for Bosch miter saw GCM 8 SJL

--- models/lib/suction-hose-connectors/library/bosch.js
+++ models/lib/suction-hose-connectors/library/bosch.js
@@ -13,6 +13,18 @@ const OrbitalSander_GSS12V13_O28 = {
     }),
 };
 
+const MiterSaw_GCM8SJL_I45 = {
+  socket:
+    construct.socket({
+      outerDiameterA: 47,
+      innerDiameterA: 46.5,
+      distanceAB: 25,
+      innerDiameterB: 45,
+      heightRingA: 5,
+    }),
+};
+
 module.exports = {
   OrbitalSander_GSS12V13_O28,
+  MiterSaw_GCM8SJL_I45,
 };
